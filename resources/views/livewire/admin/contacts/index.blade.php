df518acc4ad14696c6d07df9099179bfb7e30f42
Steve Nyanumba <reach@stevenyanumba.com>
2023-07-24 13:44:10 +0300

    Added Header tag to Contacts Livewire Component

--- resources/views/livewire/admin/contacts/index.blade.php
+++ resources/views/livewire/admin/contacts/index.blade.php
@@ -1,3 +1,8 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
+    <x-slot:header>
+        <h2 class="h4 font-weight-bold">
+            {{ __('Contacts') }}
+        </h2>
+    </x-slot:header>
+
 </div>
